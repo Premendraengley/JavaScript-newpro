388e5df4dcdec40c4b6ea4f8c88b67b5c68bd1c7
Premendraengley <126811933+Premendraengley@users.noreply.github.com>
2024-07-28 17:04:21 +0000

    28

---
+++ Basic/IIFE.js
@@ -0,0 +1,11 @@
+//define function
+
+const user = (function() {
+    console.log("hello guys");
+    
+}) ();// use ; to instruct the the funtion that is end 
+//here i used the () after the body of the funtion and also declaretion of the function
+
+(function userDetails() {
+    console.log("here we go")
+})();
---
+++ controlFlow/if-Else.js
@@ -0,0 +1,15 @@
+//normal if-else condition
+
+const user = 10;
+
+if(user==10)
+{
+    console.log("hello");
+}else{
+    console.log("go back simon");
+};
+
+
+//now i am making the condition statement with 
+//Implicit Scope
+if(user != 20)console.log("welcome sir");
